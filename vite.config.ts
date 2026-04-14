32916131d4ab1d8a01aef77bb9a75c7aa2909f7f
MG-Amazing <wang128027@gmail.com>
2026-04-14 13:33:18 +0900

    提交

--- vite.config.ts
+++ vite.config.ts
@@ -14,10 +14,15 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
+      // '/api': {
+      //   target: 'http://127.0.0.1:8080',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/api/, '')
+      // },
       '/api': {
-        target: 'http://127.0.0.1:8080',
+        target: 'https://www.dmsys-info.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/api/, '/prod-api')
       }
     }
   }
